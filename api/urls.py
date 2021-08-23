c8768f8e2f848e77dcaea62e90466504e6732dc8
fkhjoy <khjoyfoysal@gmail.com>
2021-08-24 02:29:29 +0600

    created model,api for Room

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import main
+from .views import RoomView
 
 
 urlpatterns = [
-    path('', main, name='main'),
+    path('room/', RoomView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+
+from rest_framework import generics
+
+from .models import Room
+
+from .serializers import RoomSerializer
 
 
 
 def main(request):
     return HttpResponse("<h1>Hello</h1>")
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
 
